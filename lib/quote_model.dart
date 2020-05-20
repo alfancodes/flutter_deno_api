10559e0dd2b1df86ccb40dd8929b5263cc08f00e
AlfanCodes <alfancodes@gmail.com>
2020-05-20 08:46:46 +0700

    update codes add quote model connect to API

---
+++ lib/quote_model.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Quote {
+  String quote;
+  String author;
+
+  Quote({
+    this.quote,
+    this.author,
+  });
+
+  factory Quote.createQuote(Map<String, dynamic> object) {
+    return Quote(
+      quote: object['quote'],
+      author: object['author'],
+    );
+  }
+
+  static Future<Quote> connectToAPI() async {
+    String uri = 'http://192.168.100.5:3000/api/quote';
+    var res = await http.get(uri);
+    var jsonObject = json.decode(res.body);
+    var quoteData = (jsonObject as Map<String, dynamic>)['quote'];
+
+    print(quoteData);
+
+    return Quote.createQuote(quoteData);
+  }
+}
